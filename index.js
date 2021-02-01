84038db160166ebeb549806a0c40f70e867f23e6
Miraziz <mirazizmirmuxsinovich@gmail.com>
2021-02-01 22:20:33 +0500

    qrcode servis xizmati

---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+var QRCode = require('qrcode');
+const app = express();
+let ejs = require("ejs");
+let fs = require("fs");
+let pdf = require("html-pdf");
+app.set('view engine', 'ejs')
+
+app.get("/", async (req, res) => {
+
+
+  // oy yilni taxlaymiz
+  let date = new Date();
+  let month = date.getMonth() + 1;
+  month = (month < 10 ? '0' + month : month)
+  let day = (date.getDate() < 10 ? '0' + date.getDate() : date.getDate());
+  date = day + '.' + month + '.' + date.getFullYear()
+
+  const fileName = `${+new Date()}.pdf`;
+  const pdfPath = __dirname + `/public/${fileName}`;
+console.log(fileName.substring(fileName.length-4,4))
+  
+   QRCode.toDataURL(`localhost:3000/fayllar/${fileName.substr(0,fileName.length-4)}`,(err,url)=>{
+    let options = {
+      fakultet: "Fizika-Matematika",
+      guruh: "1-1PMI19",
+      fish: "Ergashev Miraziz",
+      sabab: "Uylanganim va ishsizligim va oilaviy muammolar",
+      qr:url,
+      vaqt:new Date().toLocaleString(),
+      sana: date,
+      format: "Letter",
+      orientation: "landscape",
+      border: {
+        top: '0in', // default is 0, units: mm, cm, in, px
+        right: '0.1cm',
+        bottom: '0in',
+        left: '0in'
+      },
+    }
+  
+  
+  
+    res.render('ariza', options, (err, data) => {
+      console.log(err)
+      if (err) return res.status(404).json({
+        error: err
+      })
+  
+  
+      pdf.create(data, options).toStream(function (err, stream) {
+        if (err) return res.end(err.stack)
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', 'attachment; filename=Ariza.pdf');
+        stream.pipe(res);
+        
+  
+        
+        stream.pipe(fs.createWriteStream(pdfPath));
+  
+      });
+  
+  
+  
+    }); 
+    });
+   
+  
+ 
+  // console.log(qr)
+  // console.log(run())
+
+  
+
+  // res.download(path.join(__dirname, "/public/Ariza.pdf"));
+
+
+
+
+
+
+
+})
+
+app.get("/fayllar/:id",(req,res)=>{
+  res.download(__dirname+ `/public/${req.params.id}.pdf`);
+})
+
+
+//connection create
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (req, res) => {
+  console.log("Server is running at port 3000");
+})
---
+++ qw.js
@@ -0,0 +1,3 @@
+let a='1612198087735.pdf'
+
+console.log(a.substr(0,a.length-4))
